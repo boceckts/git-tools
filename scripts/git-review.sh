96d887195859ed1017af2545137dca449eb492ef
Tobias Boceck <boceckts@gmail.com>
2021-04-23 15:21:06 +0200

    wrap git-review into function

--- scripts/git-review.sh
+++ scripts/git-review.sh
@@ -5,75 +5,81 @@
 #               of specified commits. Review branch will default to master
 #               and number of commits to review will default to 1 if not specified.
 #               Add this script to your PATH environment variable before using it.
-# Usage:        ./git-review.sh [git-branch] [commits]
-# Examples:     ./git-review.sh
-#               ./git-review.sh develop/review-branch
-#               ./git-review.sh develop/review-branch 2
+# Usage:        git-review [git-branch] [commits]
+# Examples:     git-review
+#               git-review develop/review-branch
+#               git-review develop/review-branch 2
 
-review_branch=master
-review_commits=1
+function git-review() {
 
-# prechecks
-if [ $# -lt 1 ]
-then
-    echo "No branch to review specified, defaulting to $review_branch."
-    echo "No review range specified, defaulting to $review_commits."
-elif [ $# -eq 1 ]
-then
-    review_branch=$1
-    echo "No review range specified, defaulting to $review_commits."
-elif [ $# -eq 2 ]
-then
-    review_branch=$1
-    if [[ $2 =~ ^[0-9]{1}$ ]]
+    review_branch=master
+    review_commits=1
+
+    # prechecks
+    if [ $# -lt 1 ]
     then
-        review_commits=$2
-    else
-        echo "Invalid review range specified, defaulting to $review_commits. The review range only allows values between 0 and 9 commits."
-    fi
-else
-    review_branch=$1
-    if [[ $2 =~ ^[0-9]{1}$ ]]
+        echo "No branch to review specified, defaulting to $review_branch."
+        echo "No review range specified, defaulting to $review_commits."
+    elif [ $# -eq 1 ]
     then
-        review_commits=$2
+        review_branch=$1
+        echo "No review range specified, defaulting to $review_commits."
+    elif [ $# -eq 2 ]
+    then
+        review_branch=$1
+        if [[ $2 =~ ^[0-9]{1}$ ]]
+        then
+            review_commits=$2
+        else
+            echo "Invalid review range specified, defaulting to $review_commits. The review range only allows values between 0 and 9 commits."
+        fi
     else
-        echo "Invalid review range specified, defaulting to $review_commits. The review range only allows values between 0 and 9 commits."
+        review_branch=$1
+        if [[ $2 =~ ^[0-9]{1}$ ]]
+        then
+            review_commits=$2
+        else
+            echo "Invalid review range specified, defaulting to $review_commits. The review range only allows values between 0 and 9 commits."
+        fi
     fi
-fi
 
-git status > /dev/null 2>&1
-if [ $? -ne 0 ]
-then
-    echo "Git not available, or current directory is not a git repository. Please install git or change into a valid git directory."
-    exit 1
-fi
+    git status > /dev/null 2>&1
+    if [ $? -ne 0 ]
+    then
+        echo "Git not available, or current directory is not a git repository. Please install git or change into a valid git directory."
+        exit 1
+    fi
 
-# save working state
-original_branch=$(git branch | grep \* | cut -d " " -f 2)
-stash_message="Temporary stash for $original_branch during review of $review_branch"
-git stash push -m "$stash_message"
+    # save working state
+    original_branch=$(git branch | grep \* | cut -d " " -f 2)
+    stash_message="Temporary stash for $original_branch during review of $review_branch"
+    git stash push -m "$stash_message"
 
-# prepare review branch
-git fetch --all
-git checkout $review_branch
-## required in case of master or previous checkout
-git pull
-## use a soft reset to easily indicate changed files
-git reset --soft HEAD~$review_commits
+    # prepare review branch
+    git fetch --all
+    git checkout $review_branch
+    ## required in case of master or previous checkout
+    git pull
+    ## use a soft reset to easily indicate changed files
+    git reset --soft HEAD~$review_commits
 
-read  -n 1 -p "Waiting until review is finished, press any key to continue..."
+    read  -n 1 -p "Waiting until review is finished, press any key to continue..."
 
-# restore original state
-git reset --hard $review_branch
-git checkout $original_branch
-stash=$(git stash list | grep "$stash_message" | cut -d ":" -f 1)
-if [ -z "$stash" ]
-then
-    echo "Nothing was stashed."
-else
-    git stash pop $stash
-fi
-#git branch -D $review_branch
+    # restore original state
+    git reset --hard $review_branch
+    git checkout $original_branch
+    stash=$(git stash list | grep "$stash_message" | cut -d ":" -f 1)
+    if [ -z "$stash" ]
+    then
+        echo "Nothing was stashed."
+    else
+        git stash pop $stash
+    fi
+    #git branch -D $review_branch
 
-echo "Review finished for branch $review_branch, back on the original branch $original_branch."
-echo "Happy Coding!"
+    echo "Review finished for branch $review_branch, back on the original branch $original_branch."
+    echo "Happy Coding!"
+
+}
+
+git-review $@
