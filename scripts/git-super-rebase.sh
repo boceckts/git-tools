9281ec95df87720f4de31ec9c4f097c700608f0a
Tobias Boceck <boceckts@gmail.com>
2021-04-23 15:26:46 +0200

    wrap git-super-rebase into function

--- scripts/git-super-rebase.sh
+++ scripts/git-super-rebase.sh
@@ -5,58 +5,62 @@
 #               branch and cherry picking the number of specified commits from the
 #               temporary branch.
 #               Add this script to your PATH environment variable before using it.
-# Usage:        ./git-super-rebase.sh [git-branch] [commits]
-# Examples:     ./git-super-rebase.sh
-#               ./git-super-rebase.sh origin/master
-#               ./git-super-rebase.sh origin/master 2
+# Usage:        git-super-rebase [git-branch] [commits]
+# Examples:     git-super-rebase
+#               git-super-rebase origin/master
+#               git-super-rebase origin/master 2
 
-rebase_branch=origin/master
-rebase_commits=1
+function git-super-rebase() {
 
-# prechecks
-if [ $# -lt 1 ]
-then
-    echo "No branch to rebase specified, defaulting to $rebase_branch."
-    echo "No rebase range specified, defaulting to $rebase_commits."
-elif [ $# -eq 1 ]
-then
-    rebase_branch=$1
-    echo "No rebase range specified, defaulting to $rebase_commits."
-elif [ $# -eq 2 ]
-then
-    rebase_branch=$1
-    if [[ $2 =~ ^[0-9]{1}$ ]]
+    rebase_branch=origin/master
+    rebase_commits=1
+
+    # prechecks
+    if [ $# -lt 1 ]
     then
-        rebase_commits=$2
-    else
-        echo "Invalid rebase range specified, defaulting to $rebase_commits. The rebase range only allows values between 0 and 9 commits."
-    fi
-else
-    rebase_branch=$1
-    if [[ $2 =~ ^[0-9]{1}$ ]]
+        echo "No branch to rebase specified, defaulting to $rebase_branch."
+        echo "No rebase range specified, defaulting to $rebase_commits."
+    elif [ $# -eq 1 ]
     then
-        rebase_commits=$2
+        rebase_branch=$1
+        echo "No rebase range specified, defaulting to $rebase_commits."
+    elif [ $# -eq 2 ]
+    then
+        rebase_branch=$1
+        if [[ $2 =~ ^[0-9]{1}$ ]]
+        then
+            rebase_commits=$2
+        else
+            echo "Invalid rebase range specified, defaulting to $rebase_commits. The rebase range only allows values between 0 and 9 commits."
+        fi
     else
-        echo "Invalid rebase range specified, defaulting to $rebase_commits. The rebase range only allows values between 0 and 9 commits."
+        rebase_branch=$1
+        if [[ $2 =~ ^[0-9]{1}$ ]]
+        then
+            rebase_commits=$2
+        else
+            echo "Invalid rebase range specified, defaulting to $rebase_commits. The rebase range only allows values between 0 and 9 commits."
+        fi
     fi
-fi
 
-git status > /dev/null 2>&1
-if [ $? -ne 0 ]
-then
-    echo "Git not available, or current directory is not a git repository. Please install git or change into a valid git directory."
-    exit 1
-fi
+    git status > /dev/null 2>&1
+    if [ $? -ne 0 ]
+    then
+        echo "Git not available, or current directory is not a git repository. Please install git or change into a valid git directory."
+        exit 1
+    fi
 
-current_branch=$(git branch --show-current)
-temp_branch=_temp_$current_branch
+    current_branch=$(git branch --show-current)
+    temp_branch=_temp_$current_branch
 
-# checkout temporary branch
-git fetch --all
-git checkout -b $temp_branch
-git checkout $current_branch
-git reset --hard $rebase_branch 
-git log $temp_branch --pretty=format:'%h' -n $rebase_commits | xargs git cherry-pick --strategy=resolve
+    # checkout temporary branch
+    git fetch --all
+    git checkout -b $temp_branch
+    git checkout $current_branch
+    git reset --hard $rebase_branch 
+    git log $temp_branch --pretty=format:'%h' -n $rebase_commits | xargs git cherry-pick --strategy=resolve
 
-echo "rebase for $current_branch to $rebase_branch finished."
-echo "Happy Coding!"
+    echo "rebase for $current_branch to $rebase_branch finished."
+    echo "Happy Coding!"
+
+}
